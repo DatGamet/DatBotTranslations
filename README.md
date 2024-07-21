e8b1cba88cbafdb71c03faaf72e6a8ee5f133d35
Voroniyx <julian.ruge@icloud.com>
2024-07-21 17:26:45 +0200

    added tool

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Create a pull request or send a file to the [Support Server](https://discord.gg/
 If you find a mistake or have a suggestion for improvement, you are welcome to submit it as indicated above. I am open to suggestions. 
 
 Tip: You can use https://deepl.com/ for help. 
+
+
+## Tool
+In this repository there is also a tool which can be used to see how far a language has been translated and which keys have not yet been translated.
+
+The tool requires Node.js to be installed on your device. Then all packages can be installed with `npm install` and started with `node ./Tool/index.js`
+
+There are two optional flags that can be used:
+- `language`: To view only a specific language. Example: `node ./Tool/index.js de` for german
+- `nokeys`: Does not output any untranslated keys. Can be useful if a language has hardly been translated and you just want to get an overview of all languages.
+  Can also be used as a second flag for one language. Example: `node ./Tool/index.js de nokeys`.
---
+++ Tool/index.js
@@ -0,0 +1,80 @@
+﻿const { logger } = require("./logger");
+
+const ParentLang = "en"
+
+const locales = {
+    de: require("../de.json"),
+    en: require('../en.json'),
+    es: require("../es.json"),
+    fr: require("../fr.json"),
+    it: require("../it.json"),
+};
+
+function getNestedKeys(obj, prefix = '') {
+    return Object.keys(obj).flatMap(key => {
+        const fullPath = prefix ? `${prefix}.${key}` : key;
+        return typeof obj[key] === 'object' && obj[key] !== null ? getNestedKeys(obj[key], fullPath) : fullPath;
+    });
+}
+
+function checkTranslationExists(localeObj, keyPath) {
+    const keys = keyPath.split('.');
+    let current = localeObj;
+
+    for (let key of keys) {
+        if (current?.[key]) {
+            current = current[key];
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function getUntranslatedKeys(locale, nokeys) {
+    const localeKeys = getNestedKeys(locales[ParentLang]);
+    let translatedCount = 0;
+
+    for (let key of localeKeys) {
+        if (checkTranslationExists(locales[locale], key)) {
+            translatedCount++;
+        } else {
+            if(!nokeys) {
+                logger("info", 'I18N', `Untranslated Key (${locale}):`, key);
+            }
+        }
+    }
+
+    const percentage = (translatedCount / localeKeys.length) * 100;
+    return logger("info", 'I18N', `Translated (${locale}):`, `${percentage.toFixed(2)}%`);
+}
+
+(async () => {
+    let mode = process.argv[2];
+    let nokeys = process.argv.find(x => x.includes("nokeys")) !== undefined;
+
+    if (Object.keys(locales).includes(mode)) {
+        const locale = mode;
+
+        if (locale && locales[locale]) {
+            return getUntranslatedKeys(locale, nokeys);
+        } else {
+            return logger(
+                "warn",
+                'I18N',
+                `Cannot find "${locale ?? 'Unknown'}"! Current supported locales: ${Object.keys(locales).join(', ')}`,
+            );
+        }
+    }
+
+    if (mode !== 'all') {
+        mode = "all";
+        logger("warn", "I18N", "The tool was started with the default flag 'all'")
+    }
+
+    Object.keys(locales).forEach((key) => {
+        getUntranslatedKeys(key, nokeys);
+    });
+    return logger("success", 'I18N', `Everything done!`);
+})();
